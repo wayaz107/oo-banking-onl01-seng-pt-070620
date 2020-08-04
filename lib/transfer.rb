7ad05920ba78e3b790f45c7ccedaa60ebde3e1b9
Warda Ayaz <wayaz107@gmail.com>
2020-08-04 05:04:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,4 +10,16 @@ attr_accessor   :status, :sender, :reciever, :transfer_amount
   end
 
 
+def sender_valid?
+  account.valid?
+end
+
+def reciever_valid?
+account.valid?
+end
+
+def valid?(name)
+  sender_valid?(name) && reciever_valid?(name)
+end
+
 end
