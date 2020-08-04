53f3474616cc0211e056892360617b9c245f8e2b
Warda Ayaz <wayaz107@gmail.com>
2020-08-04 04:56:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,29 @@
 class BankAccount
 
-  attr_accessor :name, :balance, :status
+@account = []
+
+  attr_accessor :name, :balance, :status, :account
 
 def initialize(name)
   @name = name
   @balance = 1000
   @status = "open"
+end
+
+def deposit
+  @account << self
+end
+
+def display_balance
+  @balance
+end
+
+def valid?
+  status = "open" && @balance > 0
+end
+
+def close_account
+  @account.delete
+end 
+
 end
