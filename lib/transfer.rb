fc018de22f2e1fc509238a807d748c9f30c7ffc7
Warda Ayaz <wayaz107@gmail.com>
2020-08-04 06:06:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,16 @@ def valid?
 end
 
 def execute_transaction
+ if @sender.balance < @amount
+   @status = "rejected"
+   return "Transaction rejected. Please check your account balance."
+
+elsif @status == "complete"
+  puts "Transaction was already executed"
 end
+
+
+  
 
 def reverse_transfer
 end
